dbabd053553d34d33bdda980c032a099bfe1137a
Isaac Benham <isaac.benham@gmail.com>
2019-02-10 14:07:37 -0500

    define base activity

---
+++ app/src/main/java/com/example/dcdawgs_android/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.example.dcdawgs_android
+
+import android.support.v7.app.AppCompatActivity
+import com.amazonaws.mobile.config.AWSConfiguration
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient
+
+open class BaseActivity: AppCompatActivity() {
+    private var _AWSAppSyncClient: AWSAppSyncClient? = null
+
+    val awsAppSyncClient: AWSAppSyncClient
+        get() {
+            if (_AWSAppSyncClient == null) {
+                val awsConfig: AWSConfiguration = AWSConfiguration(applicationContext)
+                val builder = AWSAppSyncClient.builder()
+                _AWSAppSyncClient = builder.awsConfiguration(awsConfig).build()
+            }
+            return _AWSAppSyncClient ?: throw AssertionError("AppSyncClient Unexpectedly Null")
+        }
+
+}
--- app/src/main/java/com/example/dcdawgs_android/MainActivity.kt
+++ app/src/main/java/com/example/dcdawgs_android/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.dcdawgs_android
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
